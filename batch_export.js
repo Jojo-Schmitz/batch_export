67f74674b405312edbf732515db70de0b6130bbd
Joachim Schmitz <jojo@schmitz-digital.de>
2012-05-31 16:43:54 +0200

    Added GUI and french translation

--- batch_export.js
+++ batch_export.js
@@ -26,59 +26,65 @@
 
 // Setup some global variables
 var pluginName = qsTr("Batch Export");
-// Here you can specify which formats get exported
-// by specifying their extensions in a comma separated list
-//var outFormats = new Array("pdf", "xml");
-var outFormats = new Array("pdf");
-// Here you can specify, which format gets imported
-var inFormat = "mscz";
+var outFormats;
+var inFormats;
+var form;
+
 
 //---------------------------------------------------------
 //    init
-//    this function will be called on startup of
-//    mscore
+//    this function will be called on initial load of
+//    this plugin, currently at start of mscore
 //---------------------------------------------------------
 
 function init () {
-}
+  //QMessageBox.information(0, pluginName, "init()");
+};
+
 
 // generates a <format> file from the specified file, if no up-to-date one already exists
 // returns filename of generated file, or empty string if no file generated
-function process_one (source, outFormat) {
-  var reg = inFormat;
-  var target = source.replace(reg, outFormat);
+function process_one (source, inFormat, outFormatnotused) {
   var inFile = new QFileInfo(source);
-  var targetFile = new QFileInfo(target);
-  var doit = false;
+  var theScore; 
+  var list ="";
 
-  if (!targetFile.exists())
-    doit = true;
-  else if (targetFile.lastModified() < inFile.lastModified()) {
-    var targetHandle = new QFile(target);
-    if (targetHandle.remove())
+  for (i=0; i<outFormats.length; i++) {
+    var target = source.replace(inFormat, outFormats[i]);
+    var targetFile = new QFileInfo(target);
+    var doit = false;
+
+    if (!targetFile.exists())
       doit = true;
-    else
-      QMessageBox.warning(0, pluginName, qsTr("Unable to delete") + " " + target);
+    else if (targetFile.lastModified() < inFile.lastModified()) {
+      var targetHandle = new QFile(target);
+      if (targetHandle.remove())
+        doit = true;
+      else
+        QMessageBox.warning(0, pluginName, qsTr("Unable to delete %1") .arg(target));
+    }
+    if (doit) { // got for it
+      if (!theScore) { //( not yet open
+        theScore = new Score();
+        theScore.load(source);
+      }
+      theScore.save(target, outFormats[i]);
+
+      list += targetFile.fileName() + "\n";
+    }
   }
-  if (doit) {
-    var theScore = new Score();
-    theScore.load(source);
-    theScore.save(target, outFormat);
-    if (typeof theScore.close === 'function') // does not exist on some versions
+  if (theScore) // is a score open?
+    if (typeof theScore.close === 'function') // does not exist on 1.1 or earlier
       theScore.close();
-    return targetFile.fileName() + "\n";
-  }
-  return "";
-}
+  return list;
+};
 
 // query user for directory
 // loop through all files in folder
-// process all ".<inFormat>$" files using process_one()
-// export them to <outFormats>
-function run () {
-  var scoreList = "";
-
-  var dirString = QFileDialog.getExistingDirectory(0, "MuseScore: " + pluginName + ": " + qsTr("Select Folder"), "", 0);
+// process all ".<inFormats[i]>$" files using process_one()
+// export them to all <outFormats[j]>
+function work () {
+  var dirString = QFileDialog.getExistingDirectory(0, pluginName + qsTr(": Select Folder"), "", 0);
   if (!dirString) {
     QMessageBox.warning(0, pluginName, qsTr("No folder selected"));
     return;
@@ -86,22 +92,242 @@ function run () {
 
   var dir = new QDir(dirString);
   var dirIt = new QDirIterator(dir);
+  var scoreList = "";
 
   while (dirIt.hasNext()) {
     var file = dirIt.next();
-    if (file.match("\." + inFormat + "$"))
-      for(i=0; i<outFormats.length; i++)
-        scoreList += process_one(file, outFormats[i]);
+    for (i=0; i<inFormats.length; i++)
+      if (file.match("\." + inFormats[i] + "$"))
+        scoreList += process_one(file, inFormats[i]);
   }
 
   if (scoreList == "")
-    scoreList = qsTr("All files up to date") + "\n";
+    scoreList = qsTr("\n\nAll files are up to date\n");
   else
-    scoreList = qsTr("File(s) exported") + ":\n\n" + scoreList;
+    scoreList = qsTr("\n\nFile(s) exported:\n\n%1") .arg(scoreList);
 
-  QMessageBox.information(0, pluginName, dirString + "\n\n" + scoreList);
+  QMessageBox.information(0, pluginName, dirString + scoreList);
+};
 
-}
+function evalForm () {
+  if (form.groupBox_inFormats.checkBox_mscz.checked) inFormats.push("mscz");
+  if (form.groupBox_inFormats.checkBox_mscx.checked) inFormats.push("mscx");
+  if (form.groupBox_inFormats.checkBox_msc.checked)  inFormats.push("msc");
+  if (form.groupBox_inFormats.checkBox_xml.checked)  inFormats.push("xml");
+  if (form.groupBox_inFormats.checkBox_mxl.checked)  inFormats.push("mxl");
+  if (form.groupBox_inFormats.checkBox_mid.checked)  inFormats.push("mid");
+  if (form.groupBox_inFormats.checkBox_midi.checked) inFormats.push("midi");
+  if (form.groupBox_inFormats.checkBox_car.checked)  inFormats.push("car");
+  if (form.groupBox_inFormats.checkBox_md.checked)   inFormats.push("md");
+  if (form.groupBox_inFormats.checkBox_cap.checked)  inFormats.push("cap");
+  if (form.groupBox_inFormats.checkBox_bww.checked)  inFormats.push("bww");
+  if (form.groupBox_inFormats.checkBox_mgu.checked)  inFormats.push("mgu");
+// upercase needed too?
+//if (form.groupBox_inFormats.checkBox_MGU.checked)  inFormats.push("MGU");
+  if (form.groupBox_inFormats.checkBox_sgu.checked)  inFormats.push("sgu");
+// upper case needed too?
+//if (form.groupBox_inFormats.checkBox_SGU.checked)  inFormats.push("SGU");
+  if (form.groupBox_inFormats.checkBox_ove.checked)  inFormats.push("ove");
+  if (form.groupBox_inFormats.checkBox_scw.checked)  inFormats.push("scw");
+  if (form.groupBox_inFormats.checkBox_GTP.checked)  inFormats.push("GTP");
+  if (form.groupBox_inFormats.checkBox_GP3.checked)  inFormats.push("GP3");
+  if (form.groupBox_inFormats.checkBox_GP4.checked)  inFormats.push("GP4");
+  if (form.groupBox_inFormats.checkBox_GP5.checked)  inFormats.push("GP5");
+  if (inFormats.length == 0) {
+    QMessageBox.warning(0, pluginName, qsTr("No input format selected"));
+    exit();
+  }
+
+  if (form.groupBox_outFormats.checkBox_mscz.checked) outFormats.push("mscz");
+  if (form.groupBox_outFormats.checkBox_mscx.checked) outFormats.push("mscx");
+  if (form.groupBox_outFormats.checkBox_xml.checked)  outFormats.push("xml");
+  if (form.groupBox_outFormats.checkBox_mxl.checked)  outFormats.push("mxl");
+  if (form.groupBox_outFormats.checkBox_mid.checked)  outFormats.push("mid");
+  if (form.groupBox_outFormats.checkBox_pdf.checked)  outFormats.push("pdf");
+  if (form.groupBox_outFormats.checkBox_ps.checked)   outFormats.push("ps");
+  if (form.groupBox_outFormats.checkBox_png.checked)  outFormats.push("png");
+  if (form.groupBox_outFormats.checkBox_svg.checked)  outFormats.push("svg");
+  if (form.groupBox_outFormats.checkBox_ly.checked)   outFormats.push("ly");
+  if (form.groupBox_outFormats.checkBox_wav.checked)  outFormats.push("wav");
+  if (form.groupBox_outFormats.checkBox_flac.checked) outFormats.push("flac");
+  if (form.groupBox_outFormats.checkBox_ogg.checked)  outFormats.push("ogg");
+  if (form.groupBox_outFormats.checkBox_mp3.checked)  outFormats.push("mp3");
+  if (outFormats.length == 0) {
+    QMessageBox.warning(0, pluginName, qsTr("No output format selected"));
+    exit();
+  }
+
+  work();
+};
+
+function setDefaults () {
+  if (form) {
+    // enable/disable, depening on version
+    if ( mscoreMajorVersion >= 2) {
+      form.groupBox_inFormats.checkBox_msc.enabled =  false; // no longer supported?
+      form.groupBox_inFormats.checkBox_scw.enabled =  true;
+      form.groupBox_inFormats.checkBox_GTP.enabled =  true;
+      form.groupBox_inFormats.checkBox_GP3.enabled =  true;
+      form.groupBox_inFormats.checkBox_GP4.enabled =  true;
+      form.groupBox_inFormats.checkBox_GP5.enabled =  true;
+      form.groupBox_outFormats.checkBox_mp3.enabled = true;
+    } else {
+      form.groupBox_inFormats.checkBox_msc.enabled =  true;
+      form.groupBox_inFormats.checkBox_scw.enabled =  false;
+      form.groupBox_inFormats.checkBox_GTP.enabled =  false;
+      form.groupBox_inFormats.checkBox_GP3.enabled =  false;
+      form.groupBox_inFormats.checkBox_GP4.enabled =  false;
+      form.groupBox_inFormats.checkBox_GP5.enabled =  false;
+      form.groupBox_outFormats.checkBox_mp3.enabled = false;
+    }
+    // check/uncheck to the default, inFormats "mscz", outformats "pdf"
+    form.groupBox_inFormats.checkBox_mscz.checked =  true;
+    toggle_mscz(true); // disable corresponding outFormat
+    form.groupBox_inFormats.checkBox_mscx.checked =  false;
+    toggle_mscx(false); // enable corresponding outFormat
+    form.groupBox_inFormats.checkBox_msc.checked =   false;
+    form.groupBox_inFormats.checkBox_xml.checked =   false;
+    toggle_xml(false); // enable corresponding outFormat
+    form.groupBox_inFormats.checkBox_mxl.checked =   false;
+    toggle_mxl(false); // enable corresponding outFormat
+    form.groupBox_inFormats.checkBox_mid.checked =   false;
+    toggle_mid(false); // enable corresponding outFormat
+    form.groupBox_inFormats.checkBox_midi.checked =  false;
+    form.groupBox_inFormats.checkBox_car.checked =   false;
+    form.groupBox_inFormats.checkBox_md.checked =    false;
+    form.groupBox_inFormats.checkBox_cap.checked =   false;
+    form.groupBox_inFormats.checkBox_bww.checked =   false;
+    form.groupBox_inFormats.checkBox_mgu.checked =   false;
+  // upercase needed too?
+  //form.groupBox_inFormats.checkBox_MGU.checked =   false;
+    form.groupBox_inFormats.checkBox_sgu.checked =   false;
+  // upercase needed too?
+  //form.groupBox_inFormats.checkBox_SGU.checked =   false;
+    form.groupBox_inFormats.checkBox_ove.checked =   false;
+    form.groupBox_inFormats.checkBox_scw.checked =   false;
+    form.groupBox_inFormats.checkBox_GTP.checked =   false;
+    form.groupBox_inFormats.checkBox_GP3.checked =   false;
+    form.groupBox_inFormats.checkBox_GP4.checked =   false;
+    form.groupBox_inFormats.checkBox_GP5.checked =   false;
+    inFormats =  new Array(); // empty array, work() will fill it
+
+    form.groupBox_outFormats.checkBox_mscx.checked = false;
+    form.groupBox_outFormats.checkBox_xml.checked =  false;
+  //form.groupBox_outFormats.checkBox_xml.checked =  true;
+  //toggle_xml(true);
+    form.groupBox_outFormats.checkBox_mxl.checked =  false;
+    form.groupBox_outFormats.checkBox_mid.checked =  false;
+    form.groupBox_outFormats.checkBox_pdf.checked =  true;
+    form.groupBox_outFormats.checkBox_ps.checked =   false;
+    form.groupBox_outFormats.checkBox_png.checked =  false;
+    form.groupBox_outFormats.checkBox_svg.checked =  false;
+    form.groupBox_outFormats.checkBox_ly.checked =   false;
+    form.groupBox_outFormats.checkBox_wav.checked =  false;
+    form.groupBox_outFormats.checkBox_flac.checked = false;
+    form.groupBox_outFormats.checkBox_ogg.checked =  false;
+    form.groupBox_outFormats.checkBox_mp3.checked =  false;
+    outFormats = new Array(); // empty array, work() will fill it
+  } else {
+    // no UI, fall back to behavoir of previous version
+    inFormats =  new Array("mscz");
+  //inFormats =  new Array("mscz", "mscx");
+    outFormats = new Array("pdf");
+  //outFormats = new Array("xml", "pdf");
+  }
+};
+
+
+function toggle_mscz (enable) {
+  if (enable) {
+    form.groupBox_outFormats.checkBox_mscz.checked  = false;
+    form.groupBox_outFormats.checkBox_mscz.enabled  = false;
+ } else {
+    form.groupBox_outFormats.checkBox_mscz.enabled  = true;
+ }
+};
+
+function toggle_mscx (enable) {
+  if (enable) {
+    form.groupBox_outFormats.checkBox_mscx.checked  = false;
+    form.groupBox_outFormats.checkBox_mscx.enabled  = false;
+  } else {
+    form.groupBox_outFormats.checkBox_mscx.enabled  = true;
+  }
+};
+
+function toggle_xml (enable) {
+  if (enable) {
+    form.groupBox_outFormats.checkBox_xml.checked  = false;
+    form.groupBox_outFormats.checkBox_xml.enabled  = false;
+  } else {
+    form.groupBox_outFormats.checkBox_xml.enabled  = true;
+  }
+};
+
+function toggle_mxl (enable) {
+  if (enable) {
+    form.groupBox_outFormats.checkBox_mxl.checked  = false;
+    form.groupBox_outFormats.checkBox_mxl.enabled  = false;
+  } else {
+    form.groupBox_outFormats.checkBox_mxl.enabled  = true;
+  }
+};
+
+function toggle_mid (enable) {
+  if (enable) {
+    form.groupBox_outFormats.checkBox_mid.checked  = false;
+    form.groupBox_outFormats.checkBox_mid.enabled  = false;
+  } else {
+    form.groupBox_outFormats.checkBox_mid.enabled  = true;
+  }
+};
+
+
+function run () {
+  // read the UI file and create a form out of it
+  var loader = new QUiLoader(null);
+  var uiFile = new QFile(pluginPath + "/batch_export.ui");
+  uiFile.open(QIODevice.OpenMode(QIODevice.ReadOnly, QIODevice.Text));
+  form = loader.load(uiFile, null);
+
+  if (form) { // Todo!!
+    // initialize some widget values
+    form.windowTitle = pluginName + ": " + form.windowTitle;
+
+    setDefaults();
+    
+    // connect signals
+    form.buttonBox.accepted.connect(evalForm);
+    form.buttonBox.rejected.connect(close);
+    form.pushButton_reset.released.connect(setDefaults);
+    form.groupBox_inFormats.checkBox_mscz.toggled.connect(toggle_mscz);
+    form.groupBox_inFormats.checkBox_mscx.toggled.connect(toggle_mscx);
+    form.groupBox_inFormats.checkBox_xml.toggled.connect(toggle_xml);
+    form.groupBox_inFormats.checkBox_mxl.toggled.connect(toggle_mxl);
+    form.groupBox_inFormats.checkBox_mid.toggled.connect(toggle_mid);
+
+    // show the form
+    form.show();
+  } else {
+    // Why doesn't this work but gives an empty message?
+    //QMessageBox.warning(0, pluginName, qsTr("Can't load GUI dialog %1, continuing with default settings (%2 to %3)") .arg(uiFile) .arg(inFormats) .arg(outFormats));
+    // fallback to behavoir of the previous non-GUI version
+    setDefaults();
+    work();
+  }
+};
+
+
+//---------------------------------------------------------
+//    close
+//    this function will be called on close (unload)
+//    of this plugin, currently at close of mscore
+//    optional...
+//---------------------------------------------------------
+
+function close() {
+  //QMessageBox.information(0, pluginName, "close()");
+};
 
 
 //---------------------------------------------------------
@@ -110,9 +336,10 @@ function run () {
 //---------------------------------------------------------
 
 var mscorePlugin = {
-      menu: 'Plugins.' + pluginName,
-      init: init,
-      run:  run
-}
+  menu: 'Plugins.' + pluginName,
+  init: init,
+  run:  run,
+  onClose: close
+};
 
 mscorePlugin;
--- translations/locale_fr.ts
+++ translations/locale_fr.ts
@@ -6,140 +6,140 @@
     <message>
         <location filename="../batch_export.ui" line="17"/>
         <source>Choose Formats</source>
-        <translation>Formate wählen</translation>
+        <translation>Choisir les formats</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="45"/>
         <source>Output Formats</source>
         <comment>outFormats</comment>
-        <translation>Ausgabeformate</translation>
+        <translation>Formats de sortie</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="57"/>
         <location filename="../batch_export.ui" line="312"/>
         <source>Compressed MuseScore Format</source>
-        <translation>Komprimiertes MuseScore-Format</translation>
+        <translation>Fichier MuseScore Compressé</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="121"/>
         <source>Standard MIDI File</source>
-        <translation>MIDI-Datei</translation>
+        <translation>Fichier MIDI</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="277"/>
         <source>MP3 Audio</source>
-        <translation></translation>
+        <translation>Audio MP3</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="73"/>
         <location filename="../batch_export.ui" line="331"/>
         <source>MuseScore Format</source>
-        <translation>MuseScore-Format</translation>
+        <translation>Fichier MuseScore</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="89"/>
         <location filename="../batch_export.ui" line="363"/>
         <source>MusicXML Format</source>
-        <translation>MusicXML-Format</translation>
+        <translation>Fichier MusicXML</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="105"/>
         <location filename="../batch_export.ui" line="379"/>
         <source>Compressed MusicXML Format</source>
-        <translation>Komprimiertes MusicXML-Format</translation>
+        <translation>Fichier MusicXML Compressé</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="294"/>
         <source>Input Formats</source>
         <comment>inFormat</comment>
-        <translation>Eingabeformate</translation>
+        <translation>Format d'entrée</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="395"/>
         <source>Standard MIDI FIle</source>
-        <translation>MIDI-Datei</translation>
+        <translation>Fichier MIDI</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="143"/>
         <source>PDF File</source>
-        <translation>PDF Datei</translation>
+        <translation>Fichier PDF</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="162"/>
         <source>PostScript File</source>
-        <translation>PostScript-Datei</translation>
+        <translation>Fichier Postscript</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="178"/>
         <source>PNG Bitmap Graphic</source>
-        <translation>PNG-Bitmap-Grafik</translation>
+        <translation>Fichier PNG</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="194"/>
         <source>Scalable Vector Graphic</source>
-        <translation>Skalierbare Vektorgrafik</translation>
+        <translation>Fichier SVG</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="210"/>
         <source>LilyPond Format</source>
-        <translation>LilyPond-Format</translation>
+        <translation>Fichier Lilypond</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="226"/>
         <source>Wave Audio</source>
-        <translation></translation>
+        <translation>Audio WAVE</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="242"/>
         <source>Flac Audio</source>
-        <translation></translation>
+        <translation>Audio FLAC</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="258"/>
         <source>Ogg Vorbis Audio</source>
-        <translation></translation>
+        <translation>Fichier Ogg Vorbis</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="347"/>
         <source>Old MuseScore Format</source>
-        <translation>Altes MuseScore-Format</translation>
+        <translation>Ancien Fichier MuseScore</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="411"/>
         <location filename="../batch_export.ui" line="427"/>
         <source>MIDI Files</source>
-        <translation>MIDI-Dateien</translation>
+        <translation>Fichier MIDI</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="443"/>
         <source>MuseData Files</source>
-        <translation>Muse Data Dateien</translation>
+        <translation>Fichier MuseData</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="459"/>
         <source>Capella Files</source>
-        <translation>Capella-Dateien</translation>
+        <translation>Fichier Capella</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="475"/>
         <source>Bagpipe Music Writer Files</source>
-        <translation>Bagpipe Music Writer Dateien</translation>
+        <translation>Fichier Bagpipe Music Writer</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="491"/>
         <location filename="../batch_export.ui" line="507"/>
         <source>BB Files</source>
-        <translation>BB Dateien</translation>
+        <translation>Fichier Band in a box</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="523"/>
         <source>Overture Files</source>
-        <translation>Overture Dateien</translation>
+        <translation>Fichier Overture</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="542"/>
         <source>Score Writer Files</source>
-        <translation>Score- Writer Dateien</translation>
+        <translation>Fichier Score Writer</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="561"/>
@@ -147,12 +147,12 @@
         <location filename="../batch_export.ui" line="602"/>
         <location filename="../batch_export.ui" line="621"/>
         <source>Guitar Pro</source>
-        <translation></translation>
+        <translation>Guitar Pro</translation>
     </message>
     <message>
         <location filename="../batch_export.ui" line="638"/>
         <source>reset to defaults</source>
-        <translation>Voreinstellungen</translation>
+        <translation>Rétablir le choix par défaut</translation>
     </message>
 </context>
 <context>
@@ -160,22 +160,22 @@
     <message>
         <location filename="../batch_export.js" line="28"/>
         <source>Batch Export</source>
-        <translation>Stapel-Export</translation>
+        <translation>Export par lot</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="60"/>
         <source>Unable to delete %1</source>
-        <translation>Kann %1 nicht löschen</translation>
+        <translation>Impossible de supprimer %1</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="78"/>
         <source>: Select Folder</source>
-        <translation>: Verzeichnis wählen</translation>
+        <translation>: Sélectionner un répertoire</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="80"/>
         <source>No folder selected</source>
-        <translation>Kein Verzeichnis ausgewählt</translation>
+        <translation>Pas de répertoire sélectionné</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="97"/>
@@ -185,7 +185,7 @@ All files are up to date
 </source>
         <translation>
 
-Alle Dateien sind aktuell
+Tous les fichiers sont à jour
 </translation>
     </message>
     <message>
@@ -197,19 +197,19 @@ File(s) exported:
 %1</source>
         <translation>
 
-Datei(en) exportiert:
+Fichier(s) exporté(s):
 
 %1</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="129"/>
         <source>No input format selected</source>
-        <translation>Kein Eingabeformat gewählt</translation>
+        <translation>Pas de format d'entrée sélectionné</translation>
     </message>
     <message>
         <location filename="../batch_export.js" line="148"/>
         <source>No output format selected</source>
-        <translation>Kein Ausgabeformat gewählt</translation>
+        <translation>Pas de format de sortie sélectionné</translation>
     </message>
 </context>
 </TS>
